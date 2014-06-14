71788addb70193a7870dd187179485c16cc78bee
Browenchen <owenchen@berkeley.edu>
2014-06-14 21:56:42 +0100

    s
    s

--- random_numbers.py
+++ random_numbers.py
@@ -1,3 +1,20 @@
+"""
+Practice Techniques for Dominic O' Brien's How to have amazing memory. 
+
+
+Implementation
+Fill up dominic table. Option to assign and formulate a table.
+Memorize your table with random generated pairs using spaced repitition
+
+Next is to do strings of numbers. If we get 5 in a row correct, increase the length of the string?
+
+
+
+How to Memorize a Deck of Cards
+Epands further on the first app. Now we introduce a deck of playinng cards.  
+"""
+
+
 import random
 
 class MyClass:
@@ -10,12 +27,9 @@ m = MyClass()
 inputList = input('Enter a list: ')
 print(inputList)
 
-
 def randomNum():
 
 	list = []
-
-
 	for i in range (100):
 		list.append([str(random.randrange(0, 10)) + str(random.randrange(0, 10))])
 	m.staticNum = list
@@ -50,6 +64,7 @@ def translateNumber(newList):
 	m.transList = translatedList
 
 randomNum()
+
 translateNumber(m.staticNum)
 print(" ")
 print(m.transList)
